a9c5b37e1fb05aec68d82577246c28c3c4b7f704
中野 亮太 <rnakanor@gmail.com>
2024-06-06 18:14:22 +0900

    :hammer: Mac用にdocker-composeへ変更

--- bash.sh
+++ bash.sh
@@ -3,4 +3,4 @@
 container=${1:-bagisto-php-apache}
 # Dive into container!
 echo -e "\n\n\n\U1F433 Dive into \"${container}\" \U1F433\n\n\n"
-docker compose exec $container bash
+docker-compose exec $container bash
