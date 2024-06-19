dc6af9b759c507a553e121cab15cd91da2f96225
ryota-nakano <rnakanor@gmail.com>
2024-06-19 15:48:43 +0900

    :hammer: たぶんMac用の環境をubuntu-v1.1.0に合わせられた。はず。（テストはしてない）

--- setup.sh
+++ setup.sh
@@ -45,3 +45,6 @@ docker cp .configs/.env.testing ${apache_container_id}:/var/www/html/bagisto/.en
 
 # executing final commands
 docker exec -i ${apache_container_id} bash -c "cd bagisto && php artisan optimize:clear && php artisan migrate:fresh --seed && php artisan storage:link && php artisan bagisto:publish --force && php artisan optimize:clear"
+
+# update TrustProxies
+docker exec -i ${apache_container_id} bash -c "sed -i \"s/proxies;/proxies = '*';/\" /var/www/html/bagisto/app/Http/Middleware/TrustProxies.php"
